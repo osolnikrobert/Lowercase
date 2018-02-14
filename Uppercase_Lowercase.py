be95e1c144a697992a88e3b7044a17ff9de29f5b
Robert Osolnik <moj@email.com>
2018-02-14 21:39:35 +0100

    first commit

---
+++ Uppercase_Lowercase.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+upper = raw_input('Napišite besedilo z majhnimi črkami. ')
+lower = raw_input('Napišite besedilo z velikimi črkami. ')
+print upper.upper()
+print lower.lower()# Lowercase
